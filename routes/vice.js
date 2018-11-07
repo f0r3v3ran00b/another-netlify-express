a48f8877cb730dfe7603f57a3b3b30ed68e25c51
Sanjay Srinivas <srinivassanjay@gmail.com>
2018-11-08 09:07:29 +1100

    trying stuff

---
+++ routes/vice.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var moment = require('moment')
+
+const name = process.env.MY_NAME || `[Name not set in environment variables]`
+
+const router = express.Router();
+router.get('/vice', (req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  res.write('<h1>Wine</h1>');
+  res.end();
+});
+
+module.exports = router;
