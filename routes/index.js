2c8c5c1f52a984dfd23fb6b863f31b9dd7b0b809
Sanjay Srinivas <srinivassanjay@gmail.com>
2018-11-08 09:00:06 +1100

    trying stuff

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,14 @@
 var express = require('express');
-var router = express.Router();
 var moment = require('moment')
 
 const name = process.env.MY_NAME || `[Name not set in environment variables]`
-
 
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/html'
   });
-  res.write('<h1>Hello from Express.js!</h1>');
+  res.write('<h1>Hello from Express.js!!!!</h1>');
   res.end();
 });
 
